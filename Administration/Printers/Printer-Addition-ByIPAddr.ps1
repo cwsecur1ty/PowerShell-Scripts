bcb929c6950c8c7a498f0121e3df0f7b9e52de92
cwsecurity <csheppardoff@gmail.com>
2024-05-22 16:34:42 +0100

    Add files via upload

---
+++ Administration/Printers/Printer-Addition-ByIPAddr.ps1
@@ -0,0 +1,12 @@
+﻿# Enter printer IP & Name
+$printerIP = "0.0.0.0"
+$printerName = "Printer-Name"
+
+# Define printer Port Name and Printer Port
+$portName = "IP_$printerIP"
+Add-PrinterPort -Name $portName -PrinterHostAddress $printerIP
+
+# Add the printer
+Add-Printer -Name $printerName -PortName $portName -DriverName "Microsoft IPP Class Driver"
+
+Write-Output "Printer '$printerName' added successfully at IP address '$printerIP'."
---
+++ Administration/Printers/Printer-Removal-ByName.ps1
@@ -0,0 +1,13 @@
+﻿# Enter the name of the printer to be removed from system/s
+$printerName = "Printer-Name"
+
+# Get printer obj
+$printer = Get-Printer -Name $printerName -ErrorAction SilentlyContinue
+
+if ($printer) {
+    # Remove printer from system
+    Remove-Printer -Name $printerName
+    Write-Output "Printer '$printerName' removed successfully."
+} else {
+    Write-Output "Printer '$printerName' not found."
+}
